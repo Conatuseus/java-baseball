0c78867914b67eb2daa10ec7eed562ce456c9cbd
conatuseus <conatuseus@gmail.com>
2019-03-24 19:52:42 +0900

    Add Computer class

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,26 @@
+/*
+ *  @(#)Computer.java       1.00    2019/03/24
+ *
+ *  Copyright   (c) 2019 Myungki Sa.
+ *  Computer Science Engineering, Java, Daejeon, Korea
+ *  All rights reserved.
+ */
+
+package baseball;
+
+/**
+ * Computer Class는 컴퓨터를 위한 클래스이다.
+ * Player interface를 구현한다.
+ * 인스턴스 변수인 numbers 배열이 있다. 이 배열은 컴퓨터가 랜덤으로 생성한 세 자리의 수를 저장하기 위한 배열이다.
+ * 세 수를 생성하는 메서드가 있다.
+ *
+ * @version        1.00     2019년   3월  24일
+ * @author         사명기
+ */
+public class Computer {
+
+    /** 인스턴스 변수 numbers는 Computer가 생성한 세 자리의 수를 저장하기위한 배열 */
+    private int[] numbers;
+
+
+}
