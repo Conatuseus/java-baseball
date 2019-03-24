2643fe6a51e7dfbb7b8c9babfe4cb3dcc59460d1
conatuseus <conatuseus@gmail.com>
2019-03-25 01:23:26 +0900

    Fix constructor of Inning class

--- src/main/java/baseball/Inning.java
+++ src/main/java/baseball/Inning.java
@@ -4,8 +4,8 @@ public class Inning{
     int strike;
     int ball;
 
-    public Inning(){
-        this.strike=0;
-        this.ball=0;
+    public Inning(int strike,int ball){
+        this.strike=strike;
+        this.ball=ball;
     }
 }
