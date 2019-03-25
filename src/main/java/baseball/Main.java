3b3609358bc48eb15b947d949c2dead96339d0b2
conatuseus <conatuseus@gmail.com>
2019-03-25 13:52:51 +0900

    Add comment

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -11,6 +11,8 @@ package baseball;
 /**
  * 프로그램을 실행하기 위한 메인 클래스다.
  * Game class 객체를 생성하고, Game class의 run 메서드를 실행한다
+ * @version        1.00     2019년   3월  25일
+ * @author         사명기
  */
 public class Main {
     public static void main(String[] args) {
