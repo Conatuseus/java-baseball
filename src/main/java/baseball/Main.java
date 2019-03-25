74bbce05e143a5d37885f8461c26e330498064ac
conatuseus <conatuseus@gmail.com>
2019-03-25 11:54:31 +0900

    Fix comment

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,6 +1,13 @@
+/*
+ *  @(#)Main.java       1.00    2019/03/25
+ *
+ *  Copyright   (c) 2019 Myungki Sa.
+ *  Computer Science Engineering, Java, Daejeon, Korea
+ *  All rights reserved.
+ */
+
 package baseball;
 
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
